097f0141df555efe1704b34397572dfd3a2d94fb
Megha Thakur <megha181999@hotmail.com>
2026-08-07 20:20:43 +0530

    model trainer done

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -48,7 +48,7 @@ class ModelTrainer:
             }
 
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                             models=models,param=params)
+                                             models=models)
             
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
@@ -74,8 +74,7 @@ class ModelTrainer:
             r2_square = r2_score(y_test, predicted)
             return r2_square
             
-
-
+            logging.info(f"R2 square score: {r2_square}")
 
             
         except Exception as e:
